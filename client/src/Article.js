e5ac28da99b9b9cdd4aeeddebfa60e2696ed24e0
Tamzid Chowdhury <tamzidchowdhury@Tamzid-Chowdhury.local>
2022-01-29 03:04:22 -0500

    commit

---
+++ client/src/Article.js
@@ -0,0 +1,55 @@
+import {React, useState} from 'react';
+import { Box, Text, Grid, Center, Icon, Tooltip } from '@chakra-ui/react';
+import {motion} from 'framer-motion';
+
+const MotionBox = motion(Box);
+
+function Article(props) {
+
+    const textArray = props.article.textArr;
+    const title = props.article.title;
+    console.log(textArray)
+
+    return (
+        <MotionBox initial={{scale:0, opacity:0}} animate={{scale:1, opacity:1, duration:5}} whileHover={{ scale:1.1}} marginTop="100px" h="600px" bgColor="#1b222d" borderRadius="4%" overflow="scroll">
+            <Text bgGradient='linear(to-l, #63B3ED, #2C5282)' color="white" textAlign="center" marginTop="50px" fontSize="28px">{title}</Text>
+            <Text margin="30px" color="white" fontSize="20px">
+                {
+                    textArray.map((text) => {
+                    if(text.type == "normal"){
+                        return (
+                            <span>{text.text}</span>
+                        )
+                    }
+                    if(text.type == "extraction"){
+                            return (
+                                <MotionBox display="inline" color="blue.600">
+                                    {
+                                        text.extArr.map((text) => {
+                                            if(text.extType == "slot"){
+                                                return (
+                                                    <MotionBox whileHover={{ fontSize:"24px"}} bgGradient='linear(to-l, #63B3ED, #2C5282)' bgColor="blue.500" color="white" as="span">
+                                                        {text.text}
+                                                        <MotionBox as="span" fontSize="10px" bgColor="white" color="black" margin="5px">  {text.slotValue}</MotionBox>
+                                                    </MotionBox>
+                                                    )
+                                            }
+                                            else if(text.extType == "nonslot"){
+                                                return (
+                                                    <MotionBox  as="span">{text.text}</MotionBox>
+                                                    )
+                                            }
+
+                                        })
+                                    }
+                                </MotionBox>
+                            )
+                    }
+                    })   
+                }
+            </Text>
+        </MotionBox>
+    )
+}
+
+export default Article;
--- client/src/TestPage.js
+++ client/src/TestPage.js
@@ -1,56 +1,21 @@
 import {React, useState} from 'react';
 import { Box, Text, Grid, Center, Icon, Tooltip } from '@chakra-ui/react';
-import {article1} from "./document.json";
+import {article1} from "./article1.json";
+import {article2} from "./article2.json";
 import {motion} from 'framer-motion';
+import Article from './Article'
 
 const MotionBox = motion(Box);
 
 function TestPage() {
 
-    const textArray = article1.textArr;
-    console.log(textArray)
 
     return (
-        <Grid templateColumns="4fr 5fr 4fr">
+        <Grid templateColumns="2fr 6fr 1fr 6fr 2fr">
             <Box></Box>
-            <MotionBox whileHover={{ scale:1.1}} marginTop="100px" h="600px" bgColor="#1b222d" borderRadius="4%" overflow="scroll">
-                <Text bgGradient='linear(to-l, #63B3ED, #2C5282)' color="white" textAlign="center" marginTop="50px" fontSize="28px">Uniswap Labs Report No. 24</Text>
-                <Text margin="30px" color="white" fontSize="20px">
-                    {
-                     textArray.map((text) => {
-                        if(text.type == "normal"){
-                            return (
-                                <span>{text.text}</span>
-                            )
-                        }
-                        if(text.type == "extraction"){
-                                return (
-                                    <MotionBox display="inline" color="blue.600">
-                                        {
-                                            text.extArr.map((text) => {
-                                                if(text.extType == "slot"){
-                                                    return (
-                                                        <MotionBox whileHover={{ fontSize:"24px"}} bgGradient='linear(to-l, #63B3ED, #2C5282)' bgColor="blue.500" color="white" as="span">
-                                                            {text.text}
-                                                            <Box as="span" fontSize="10px" bgColor="white" color="black" margin="5px">  {text.slotValue}</Box>
-                                                        </MotionBox>
-                                                     )
-                                                }
-                                                else if(text.extType == "nonslot"){
-                                                    return (
-                                                        <MotionBox  as="span">{text.text}</MotionBox>
-                                                     )
-                                                }
-
-                                            })
-                                        }
-                                    </MotionBox>
-                                )
-                        }
-                     })   
-                    }
-                </Text>
-            </MotionBox>
+            <Article article={article1}/>
+            <Box></Box>
+            <Article article={article2}/>
             <Box></Box>
         </Grid>
     );
