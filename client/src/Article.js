989471cadc92589846a00a36d10716f24d84250d
Tamzid Chowdhury <tamzidchowdhury@Tamzid-Chowdhury.local>
2022-01-29 03:37:01 -0500

    final

--- client/src/Article.js
+++ client/src/Article.js
@@ -11,7 +11,7 @@ function Article(props) {
     console.log(textArray)
 
     return (
-        <MotionBox initial={{scale:0, opacity:0}} animate={{scale:1, opacity:1, duration:5}} whileHover={{ scale:1.1}} marginTop="100px" h="600px" bgColor="#1b222d" borderRadius="4%" overflow="scroll">
+        <MotionBox initial={{scale:0, opacity:0}} animate={{scale:1, opacity:1, duration:5}} whileHover={{ scale:1.1}} marginTop="60px" h="600px" bgColor="#1b222d" borderRadius="4%" overflow="scroll">
             <Text bgGradient='linear(to-l, #63B3ED, #2C5282)' color="white" textAlign="center" marginTop="50px" fontSize="28px">{title}</Text>
             <Text margin="30px" color="white" fontSize="20px">
                 {
