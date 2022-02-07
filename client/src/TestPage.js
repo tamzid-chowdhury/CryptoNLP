ad825cf8d3dcdcdb8fbf4a30b29a4ed817dceacb
Tamzid Chowdhury <tamzidchowdhury@Tamzid-Chowdhury.local>
2022-02-07 06:59:42 -0500

    lol

--- client/src/TestPage.js
+++ client/src/TestPage.js
@@ -21,11 +21,11 @@ const MotionBox = motion(Box);
 function TestPage() {
 
     const {ref, inView} = useInView({
-        threshold: 0.2
+        threshold: 0.3
     });
 
     const {ref:ref1, inView:inView1} = useInView({
-        threshold: 0.2
+        threshold: 0.3
     });
 
     const [showGraphs, setShowGraphs] = useState(false);
@@ -69,45 +69,45 @@ function TestPage() {
     const animation1 = useAnimation();
     const animation2 = useAnimation();
 
-    useEffect(() => {
-        if(inView){
-            animation.start({
-                scale: 1.1,
-                y: 0,
-                transition: {
-                    type: "spring",
-                    duration: 1,
-                    ease: [0.83, 0, 0.17, 1],
-                    y: {
-                        delay: 0.1
-                    }
-                }
-            })
-            animation1.start({
-                scale: 1.1,
-                y: 0,
-                transition: {
-                    type: "spring",
-                    duration: 1,
-                    ease: [0.83, 0, 0.17, 1],
-                    y: {
-                        delay: 0.1
-                    }
-                }
-            })
-        }
-        if(!inView){
-            animation.start({
-                scale: 0.8,
-                y:0
-            })
-            animation1.start({
-                scale: 0.8,
-                y:0
-            })
-        }
-        console.log("use effect inView=", inView)
-    },[inView])
+    // useEffect(() => {
+    //     if(inView){
+    //         animation.start({
+    //             scale: 1.1,
+    //             y: 0,
+    //             transition: {
+    //                 type: "spring",
+    //                 duration: 1,
+    //                 ease: [0.83, 0, 0.17, 1],
+    //                 y: {
+    //                     delay: 0.1
+    //                 }
+    //             }
+    //         })
+    //         animation1.start({
+    //             scale: 1.1,
+    //             y: 0,
+    //             transition: {
+    //                 type: "spring",
+    //                 duration: 1,
+    //                 ease: [0.83, 0, 0.17, 1],
+    //                 y: {
+    //                     delay: 0.1
+    //                 }
+    //             }
+    //         })
+    //     }
+    //     if(!inView){
+    //         animation.start({
+    //             scale: 0.8,
+    //             y:0
+    //         })
+    //         animation1.start({
+    //             scale: 0.8,
+    //             y:0
+    //         })
+    //     }
+    //     console.log("use effect inView=", inView)
+    // },[inView])
 
     const leftGraphVariants = {
         hidden: {
@@ -265,13 +265,13 @@ function TestPage() {
             </Grid>
             <Grid templateColumns="2fr 8fr 2fr 8fr 2fr" h="65vh">
                 <Box></Box>
-                <MotionBox whileHover={{ scale:1.2}} animate={animation} ref={ref} w="38vw" h="45vh" marginTop="10px" bgColor="#202835" borderRadius="4%">
+                <MotionBox whileHover={{ scale:1.2}}  ref={ref} w="38vw" h="45vh" marginTop="10px" bgColor="#202835" borderRadius="4%">
                     <Box margin="25px">
                     {showGraphs && <BarChart1></BarChart1>}
                     </Box>
                 </MotionBox>
                 <Box></Box>
-                <MotionBox whileHover={{ scale:1.2}} animate={animation1} ref={ref} w="38vw" h="45vh" marginTop="10px" bgColor="#1b222d" borderRadius="4%">
+                <MotionBox whileHover={{ scale:1.2}} ref={ref} w="38vw" h="45vh" marginTop="10px" bgColor="#1b222d" borderRadius="4%">
                     <Box margin="25px">
                     {showGraphs && <BarChart2></BarChart2>}
                     </Box>
