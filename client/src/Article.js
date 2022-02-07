64cd97d142e8e59f2352d190483de3cdcebb7efe
Tamzid Chowdhury <tamzid.chowdhury@stonybrook.edu>
2022-02-07 05:36:11 -0500

    done

--- client/src/Article.js
+++ client/src/Article.js
@@ -77,7 +77,7 @@ function Article(props) {
     console.log(extractions)
 
     return (
-        <MotionBox initial={{scale:0, opacity:0}} animate={{scale:1, opacity:1}} transition={{duration:0.2}} whileHover={{ scale:1.1}} marginTop="6vh" w="38vw" h="80vh" bgColor="#1b222d" borderRadius="4%" overflow="scroll">
+        <MotionBox initial={{scale:0, opacity:0}} animate={{scale:1, opacity:1}} transition={{duration:0.25}} whileHover={{ scale:1.1}} marginTop="6vh" w="38vw" h="80vh" bgColor="#1b222d" borderRadius="4%" overflow="scroll">
             {view == "full" ? 
             <Box>
             <Button colorScheme="blue" top="8px" left="15px" size='xs' onClick={() => {setView("extractions")}}>
--- client/src/TestPage.js
+++ client/src/TestPage.js
@@ -9,6 +9,13 @@ import BarChart1 from './BarChart1'
 import BarChart2 from './BarChart2'
 import { Bar } from 'react-chartjs-2';
 import { useInView } from 'react-intersection-observer'
+import {faker} from '@faker-js/faker'
+import "react-tabulator/lib/styles.css"; // default theme
+import "react-tabulator/css/materialize/tabulator_materialize.min.css"; // meterialize
+
+// for React 16.4.x use: import { ReactTabulator }
+import { ReactTabulator } from "react-tabulator"; // for React 15.x
+
 
 const MotionBox = motion(Box);
 
@@ -18,7 +25,36 @@ function TestPage() {
         threshold: 0.2
     });
 
+    const {ref:ref1, inView:inView1} = useInView({
+        threshold: 0.2
+    });
+
     const [showGraphs, setShowGraphs] = useState(false);
+
+    useEffect(() => {
+        if(inView1){
+            animation2.start({
+                scale: 1,
+                opacity: 1,
+                transition: {
+                    type: "spring",
+                    duration: 1,
+                    ease: [0.83, 0, 0.17, 1],
+                    y: {
+                        delay: 0.1
+                    }
+                }
+            })
+        }
+        if(!inView1){
+            animation2.start({
+                scale: 0.8,
+                opacity:0
+            })
+        }
+        
+    }, [inView1])
+
 
     useEffect(() => {
         if(inView){
@@ -32,6 +68,7 @@ function TestPage() {
 
     const animation = useAnimation();
     const animation1 = useAnimation();
+    const animation2 = useAnimation();
 
     useEffect(() => {
         if(inView){
@@ -111,6 +148,43 @@ function TestPage() {
         }
       };
 
+      const columns = [
+        { title: "Name", field: "name", width: 150, minWidth: 100, responsive: 0 },
+        { title: "Favourite Color", field: "color", minWidth: 100, responsive: 2 },
+        { title: "Date Of Birth", field: "dob", minWidth: 100, responsive: 2 },
+        {
+          title: "Passed?",
+          field: "passed",
+          align: "center",
+          formatter: "tickCross",
+          minWidth: 100,
+          responsive: 0
+        }
+      ];
+
+      const generator = (schema, min = 1, max) => {
+        max = max || min;
+        return Array.from({ length: faker.random.number({ min, max }) }).map(() =>
+          Object.keys(schema).reduce((entity, key) => {
+            entity[key] = faker.fake(schema[key]);
+            return entity;
+          }, {})
+        );
+      };
+
+      const clientsSchema = {
+        name: "{{name.firstName}}",
+        color: "{{internet.color}}",
+        dob: "{{date.recent}}",
+        passed: "{{random.boolean}}"
+      };
+      
+      const data = generator(clientsSchema, 10, 10);
+
+      const options = {
+        movableRows: false,
+        responsiveLayout: "hide"
+      };
 
 
     return (
@@ -122,7 +196,7 @@ function TestPage() {
                 <Article article={article2}/>
                 <Box></Box>
             </Grid>
-            <Grid templateColumns="2fr 8fr 2fr 8fr 2fr">
+            <Grid templateColumns="2fr 8fr 2fr 8fr 2fr" h="65vh">
                 <Box></Box>
                 <MotionBox whileHover={{ scale:1.2}} animate={animation} ref={ref} w="38vw" h="45vh" marginTop="10px" bgColor="#202835" borderRadius="4%">
                     <Box margin="25px">
@@ -133,6 +207,21 @@ function TestPage() {
                 <MotionBox whileHover={{ scale:1.2}} animate={animation1} ref={ref} w="38vw" h="45vh" marginTop="10px" bgColor="#1b222d" borderRadius="4%">
                     <Box margin="25px">
                     {showGraphs && <BarChart2></BarChart2>}
+                    </Box>
+                </MotionBox>
+                <Box></Box>
+            </Grid>
+            <Grid templateColumns="2fr 20fr 2fr" h="40vh">
+                <Box></Box>
+                <MotionBox ref={ref1} animate={animation2} whileHover={{scale:1.1}} color="#4b8ae1" bgColor="#1b222d" borderRadius="2%" h="40vh" overflow="scroll">
+                    <Box margin="30px">
+                    <ReactTabulator
+                        columns={columns}
+                        data={data}
+                        options={options}
+                        data-custom-attr="test-custom-attribute"
+                        className="custom-css-class"
+                        />
                     </Box>
                 </MotionBox>
                 <Box></Box>
